ad9a2229782e1e057925b3f96edeb888d168364a
DESKTOP-B8C8MKU\sehunKim <sehun5515@pukyong.ac.kr>
2018-03-19 11:04:32 +0900

    Problem1 answer

---
+++ Week3/Problem1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+int main() {
+	int number;
+	int sum_even = 0, sum_odd = 0;
+	int inputCount = 0;
+
+	for (;;) {
+		scanf("%d", &number);
+		inputCount++;
+
+		if (number % 2 == 0) {
+			sum_even += number;
+		}
+		else sum_odd += number;
+
+		if (sum_even == sum_odd) {
+			printf("%d\n", inputCount);
+			break;
+		}
+	}
+}
